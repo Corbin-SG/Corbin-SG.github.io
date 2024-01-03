f652df69566195783b6d28aa80929302d3921a83
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2024-01-03 13:40:20 -0500

    interact with smiley

---
+++ interact.js
@@ -0,0 +1,6 @@
+// Identifying our image
+const smiley = document.querySelector("#smiley");
+// Adding reaction when image is clicked
+smiley.addEventListener("click", () => {
+	alert(`Smiley says "You clicked me! :0"`);
+});
